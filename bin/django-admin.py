3e23cd7b4af870e061dffa5843fdcb12a5423d05
FlawlessLF <saharalm4800@gmail.com>
2020-10-08 03:59:25 +0300

    main

---
+++ bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/saharalm/Documents/GitHub/a basic web app /a-web-application/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
